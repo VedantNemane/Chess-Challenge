6d1c1f5ae8fe8cc46655dd93244a3af8fde54a87
VedantNemane <vn295@bath.ac.uk>
2023-07-31 22:36:36 +0100

    some code

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,22 +1,41 @@
 ﻿using ChessChallenge.API;
 using Microsoft.CodeAnalysis;
+using Raylib_cs;
+using System;
+using System.Threading.Tasks.Sources;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
-         Move[] moves = board.GetLegalMoves();
-         return moves[0];
+        // Console.WriteLine();
+        for (int i = 0; i < board.GetAllPieceLists().Length; i++)
+        {
+            Console.Write($" ==> {board.GetAllPieceLists()[i]}");
+        }
+        Console.ReadKey();
+        Move[] moves = board.GetLegalMoves();
+        return moves[0];
     }
 
-    public static bool minimax(Board board, int depth, int maximisingPlayer) { 
+    public static float evaluate(Board board, int depth) {
+        float score = 0;
+
+        
+
+        return score;
+    }
+
+    public static float minimax(Board board, int depth, bool maximisingPlayer) { 
         if (!(depth != 0 && !board.IsInCheckmate() && !board.IsInsufficientMaterial() && !board.IsInCheck()))
         {
-            return true;
+            
         }
 
         if (maximisingPlayer) { 
             
         }
+
+        return 0.0f;
     }
 }
